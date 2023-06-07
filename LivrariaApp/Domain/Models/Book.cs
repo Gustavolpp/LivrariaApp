f501bff84a21792e615c1a0aa3053e0027d5db54
Gustavo Maldonado <gustavolpp@gmail.com>
2023-06-06 22:19:22 -0300

    Corrigindo nomenclatura do autor

--- LivrariaApp/Domain/Models/Book.cs
+++ LivrariaApp/Domain/Models/Book.cs
@@ -5,7 +5,7 @@ namespace LivrariaApp.Domain.Models
     public class Book : Entity
     {
         public string Name { get; set; }
-        public string Actor { get; set; }
+        public string Author { get; set; }
         public string Description { get; set; }
         public DateTime RegisterDate { get; set; }
     }
