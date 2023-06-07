2d0e27e28b5e553686c8f93d78450ba50fab73f0
Gustavo Maldonado <gustavolpp@gmail.com>
2023-06-06 21:52:45 -0300

    Criação do BooksController

---
+++ LivrariaApp/Controllers/BooksController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LivrariaApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            
+            return View();
+        }
+    }
+}
