defb6069744d6f23b8463ffc7f364e67d953894c
Gustavo Maldonado <gustavolpp@gmail.com>
2023-06-06 21:53:46 -0300

    Criação do BookService com CRUD

---
+++ LivrariaApp/Application/Services/BookService.cs
@@ -0,0 +1,42 @@
+﻿using LivrariaApp.Domain.Interfaces.Repositories;
+using LivrariaApp.Domain.Interfaces.Services;
+using LivrariaApp.Domain.Models;
+
+namespace LivrariaApp.Application.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly IBookRepository _repository;
+        public BookService(IBookRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<IEnumerable<Book>> GetAll()
+        {
+            return await _repository.GetAll();
+        }
+
+        public async Task<Book> GetById(long id)
+        {
+            var book = await _repository.SelectAsync(id);
+            return book;
+        }
+
+        public async Task<Book> Create(Book model)
+        {
+            var book = await _repository.InsertAsync(model);
+            return book;
+        }
+
+        public async Task<Book> Update(Book model)
+        {
+            var book = await _repository.UpdateAsync(model);
+            return book;
+        }
+        public async Task<bool> Delete(long id)
+        {
+            return await _repository.DeleteAsync(id);
+        }
+    }
+}
---
+++ LivrariaApp/Domain/Interfaces/Services/IBookService.cs
@@ -0,0 +1,13 @@
+﻿using LivrariaApp.Domain.Models;
+
+namespace LivrariaApp.Domain.Interfaces.Services
+{
+    public interface IBookService
+    {
+        Task<Book> GetById(long id);
+        Task<IEnumerable<Book>> GetAll();
+        Task<Book> Create(Book model);
+        Task<Book> Update(Book model);
+        Task<bool> Delete(long id);
+    }
+}
